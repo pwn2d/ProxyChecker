f698e325f9f4ccd7c3c82244d4a014b8db9f4615
pwn2d <88736844+pwn2d@users.noreply.github.com>
2021-08-27 17:25:50 +1400

    Add files via upload

---
+++ proxychecker.py
@@ -0,0 +1,14 @@
+import proxy
+
+def main():
+    proxy.Download_Socks4_Proxies()
+    proxy.Check_Socks_Proxies()
+    proxy.Append_Working_Proxies_to_File()
+
+main()
+
+# => puts proxies into proxy.proxies
+# => puts working proxies into proxy.working_proxies
+# => both of which are lists
+# pwn3d
+
